133226de283c1ce53e261fad21d25618aabb1678
Leo <lcheung@swellbottle.com>
2021-12-29 12:31:44 -0500

    restore task checkbox button

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,20 +6,19 @@ import 'package:todoey/models/task_data.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-// wrap any downstream widgets with Consumer so it has easy access to provider variables
+    // wrap any downstream widgets with Consumer so it has easy access to provider variables
     return Consumer<TaskData>(
       // name the provider variable "taskData"
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+
             return TaskTile(
-              isChecked: taskData.tasks[index].isDone,
-              taskTitle: taskData.tasks[index].name,
+              isChecked: task.isDone,
+              taskTitle: task.name,
               checkboxCallback: (newValue) {
-                // setState(() {
-                //   // add "!" exclamation mark after newValue to check for null safety
-                //   Provider.of<TaskData>(context).tasks[index].isDone = newValue!;
-                // });
+                taskData.updateTask(task);
               },
             );
           },
