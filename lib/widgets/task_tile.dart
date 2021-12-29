56e4bee1f76ca0aaaaac6a5304a7d547b00b48e0
Leo <lcheung@swellbottle.com>
2021-12-29 09:07:15 -0500

    put all tasks into a task list and dynamically populate them with ListView.builder

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -3,62 +3,36 @@ import 'package:flutter/material.dart';
 // StateWidget - state object it’s coupled with, is mutable and allows the rebuild of the widget whenever the state changes.
 // StatelessWidget - all of the widget’s properties are immutable.
 
-// convert StatelessWidget to StatefulWidget
-class TaskTile extends StatefulWidget {
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
+class TaskTile extends StatelessWidget {
+  TaskTile({required this.isChecked, required this.taskTitle});
 
-// This is the state object
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
-
-  // using callback to change parent properties value
-  // void checkboxCallback(bool? newValue) {
-  //   setState(() {
-  //     // add "!" exclamation mark after newValue to check for null safety
-  //     isChecked = newValue!;
-  //   });
-  // }
+  final bool isChecked;
+  final String taskTitle;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'Task Item 1',
+        taskTitle,
         style: TextStyle(
           // hide and show the line strike based on isChecked value
           decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
       // passing parent value to child
-      trailing: TaskCheckbox(
-        isChecked,
-        // checkboxCallback,
-        (bool? newValue) {
-          setState(() {
-            // add "!" exclamation mark after newValue to check for null safety
-            isChecked = newValue!;
-          });
-        },
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: (newvalue) {},
       ),
     );
   }
 }
 
-// all properties needed to be final and can't be changed in a StatelessWidget
-class TaskCheckbox extends StatelessWidget {
-  TaskCheckbox(this.checkboxState, this.changeFunc);
-
-  final bool checkboxState;
-  final Function(bool?) changeFunc;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: checkboxState,
-      activeColor: Colors.lightBlueAccent,
-      onChanged: changeFunc,
-    );
-  }
-}
+//
+// (bool? newValue) {
+//           setState(() {
+//             // add "!" exclamation mark after newValue to check for null safety
+//             isChecked = newValue!;
+//           });
+//         },
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
+import 'package:todoey/models/task.dart';
 
-class TasksList extends StatelessWidget {
+// convert this to a stateful widget so we can update the tasks
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  // set up list to hold all tasks
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      TaskTile(),
-      TaskTile(),
-      TaskTile(),
-      TaskTile(),
-    ]);
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          isChecked: tasks[index].isDone,
+          taskTitle: tasks[index].name,
+        );
+      },
+      itemCount: tasks.length,
+    );
   }
 }
